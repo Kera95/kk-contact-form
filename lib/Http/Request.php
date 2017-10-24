caef3b42dec1f6c2e8dadfcf52f42df211a247cb
Kera <karalickerim95@gmail.com>
2017-10-24 23:16:29 +0200

    Serialized the fields in validateFields

--- lib/Http/Request.php
+++ lib/Http/Request.php
@@ -12,10 +12,19 @@ use KKCForm\Http\RequestInterface;
 
 class Request implements RequestInterface
 {
+    private $fieldNamePrefix = '';
     private $postArray = [];
+    private $serializedArray = [];
 
     function __construct()
     {
+        $this->fieldNamePrefix = 'kkwpcf';
+        $this->serializedArray = [
+            'name' => [],
+            'email' => [],
+            'subject' => [],
+            'message' => []
+        ];
         if ( $_POST ) {
             $this->postArray = $_POST;
         }
@@ -28,15 +37,95 @@ class Request implements RequestInterface
     }
 
     public function executeForm() {
-        // TODO: Execute the form and update options with form field settings (For frontend)
         if ( $this->postArray ) {
             $this->validateFields( $this->postArray );
         }
     }
 
-    public function validateFields(array $fields)
-    {
-        // TODO: Implement validateFields() method.
+    /**
+     * Validates the passed fields
+     * @param array $fields
+     */
+    public function validateFields(array $fields){
 
+        check_admin_referer( 'kkcf_form_fields_validate', 'kkcf_form_fields_refferer' );
+
+        if ( $fields ) {
+            echo "<pre>";
+            var_dump( $fields );
+            echo "</pre>";
+
+            $this->serializeArray( $fields );
+
+            if ( $this->serializedArray ) {
+                echo "<pre>";
+                foreach ( $this->serializedArray as $formFieldArray => $formFields ) {
+                    $formFieldArray = (string)$formFieldArray;
+                    // TODO: Validate fields
+                    switch ( $formFieldArray ) {
+                        case "name": {
+                            break;
+                        }
+                        case "email": {
+                            break;
+                        }
+                        case "subject": {
+                            break;
+                        }
+                        case "message": {
+                            break;
+                        }
+                        default: {}
+                    }
+                }
+                echo "</pre>";
+            }
+        }
+    }
+
+    /**
+     * Serializes the fields array
+     * Takes one argument, array
+     * Serializes the array in the way it filters the fields by form field
+     * @param array $array
+     * @return bool
+     */
+    private function serializeArray(array $array) {
+        if ( $array ) {
+            foreach ( $array as $key => $value ) {
+                if ( $key ) {
+                    $parsedKey = explode( "_", $key );
+                    if ( $parsedKey[0] === $this->fieldNamePrefix ) {
+                        if ( $parsedKey[2] ) {
+                            $parsedKey[2] = (string)$parsedKey[2];
+                            switch ( $parsedKey[2] ) {
+                                case "name": {
+                                    $keyName = $this->fieldNamePrefix."_".$parsedKey[1]."_".$parsedKey[2];
+                                    $this->serializedArray['name'][$keyName] = $value;
+                                    break;
+                                }
+                                case "email": {
+                                    $keyName = $this->fieldNamePrefix."_".$parsedKey[1]."_".$parsedKey[2];
+                                    $this->serializedArray['email'][$keyName] = $value;
+                                    break;
+                                }
+                                case "subject": {
+                                    $keyName = $this->fieldNamePrefix."_".$parsedKey[1]."_".$parsedKey[2];
+                                    $this->serializedArray['subject'][$keyName] = $value;
+                                    break;
+                                }
+                                case "message": {
+                                    $keyName = $this->fieldNamePrefix."_".$parsedKey[1]."_".$parsedKey[2];
+                                    $this->serializedArray['message'][$keyName] = $value;
+                                    break;
+                                }
+                                default: {}
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
--- resources/views/kkwpcf-main.php
+++ resources/views/kkwpcf-main.php
@@ -37,80 +37,80 @@
                                     <div id="one" class="form-field-group">
                                         <h4>Name field:</h4>
                                         <div class="input-field">
-                                            <label for="kkwpcfClassName">Class</label>
-                                            <input type="text" name="kkwpcfClassName" id="kkwpcfClassName" placeholder="Class">
+                                            <label for="kkwpcf_class_name">Class</label>
+                                            <input type="text" name="kkwpcf_class_name" id="kkwpcf_class_name" placeholder="Class">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfIdName">Id</label>
-                                            <input type="text" name="kkwpcfIdName" id="kkwpcfIdName" placeholder="Id">
+                                            <label for="kkwpcf_id_name">Id</label>
+                                            <input type="text" name="kkwpcf_id_name" id="kkwpcf_id_name" placeholder="Id">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfMaxlenName">Max length</label>
-                                            <input type="number" min="1" max="99" name="kkwpcfMaxlenName" id="kkwpcfMaxlenName" placeholder="Max length">
+                                            <label for="kkwpcf_maxlen_name">Max length</label>
+                                            <input type="number" min="1" max="99" name="kkwpcf_maxlen_name" id="kkwpcf_maxlen_name" placeholder="Max length">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfPlaceholderName">Placeholder</label>
-                                            <input type="text" name="kkwpcfPlaceholderName" id="kkwpcfPlaceholderName" placeholder="Placeholder">
+                                            <label for="kkwpcf_placeholder_name">Placeholder</label>
+                                            <input type="text" name="kkwpcf_placeholder_name" id="kkwpcf_placeholder_name" placeholder="Placeholder">
                                         </div>
                                     </div>
 
                                     <div id="two" class="form-field-group">
                                         <h4>Email field:</h4>
                                         <div class="input-field">
-                                            <label for="kkwpcfClassEmail">Class</label>
-                                            <input type="text" name="kkwpcfClassEmail" id="kkwpcfClassEmail" placeholder="Class">
+                                            <label for="kkwpcf_class_email">Class</label>
+                                            <input type="text" name="kkwpcf_class_email" id="kkwpcf_class_email" placeholder="Class">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfIdEmail">Id</label>
-                                            <input type="text" name="kkwpcfIdEmail" id="kkwpcfIdEmail" placeholder="Id">
+                                            <label for="kkwpcf_id_email">Id</label>
+                                            <input type="text" name="kkwpcf_id_email" id="kkwpcf_id_email" placeholder="Id">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfMaxlenEmail">Max length</label>
-                                            <input type="number" min="1" max="99" name="kkwpcfMaxlenEmail" id="kkwpcfMaxlenEmail" placeholder="Max length">
+                                            <label for="kkwpcf_maxlen_email">Max length</label>
+                                            <input type="number" min="1" max="99" name="kkwpcf_maxlen_email" id="kkwpcf_maxlen_email" placeholder="Max length">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfPlaceholderEmail">Placeholder</label>
-                                            <input type="text" name="kkwpcfPlaceholderEmail" id="kkwpcfPlaceholderEmail" placeholder="Placeholder">
+                                            <label for="kkwpcf_placeholder_email">Placeholder</label>
+                                            <input type="text" name="kkwpcf_placeholder_email" id="kkwpcf_placeholder_email" placeholder="Placeholder">
                                         </div>
                                     </div>
 
                                     <div id="three" class="form-field-group">
                                         <h4>Subject field:</h4>
                                         <div class="input-field">
-                                            <label for="kkwpcfClassSubject">Class</label>
-                                            <input type="text" name="kkwpcfClassSubject" id="kkwpcfClassSubject" placeholder="Class">
+                                            <label for="kkwpcf_class_subject">Class</label>
+                                            <input type="text" name="kkwpcf_class_subject" id="kkwpcf_class_subject" placeholder="Class">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfIdSubject">Id</label>
-                                            <input type="text" name="kkwpcfIdSubject" id="kkwpcfIdSubject" placeholder="Id">
+                                            <label for="kkwpcf_id_subject">Id</label>
+                                            <input type="text" name="kkwpcf_id_subject" id="kkwpcf_id_subject" placeholder="Id">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfMaxlenSubject">Max length</label>
-                                            <input type="number" min="1" max="99" name="kkwpcfMaxlenSubject" id="kkwpcfMaxlenSubject" placeholder="Max length">
+                                            <label for="kkwpcf_maxlen_subject">Max length</label>
+                                            <input type="number" min="1" max="99" name="kkwpcf_maxlen_subject" id="kkwpcf_maxlen_subject" placeholder="Max length">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfPlaceholderSubject">Placeholder</label>
-                                            <input type="text" name="kkwpcfPlaceholderSubject" id="kkwpcfPlaceholderSubject" placeholder="Placeholder">
+                                            <label for="kkwpcf_placeholder_subject">Placeholder</label>
+                                            <input type="text" name="kkwpcf_placeholder_subject" id="kkwpcf_placeholder_subject" placeholder="Placeholder">
                                         </div>
                                     </div>
 
                                     <div id="four" class="form-field-group">
                                         <h4>Message field:</h4>
                                         <div class="input-field">
-                                            <label for="kkwpcfClassMessage">Class</label>
-                                            <input type="text" name="kkwpcfClassMessage" id="kkwpcfClassMessage" placeholder="Class">
+                                            <label for="kkwpcf_class_message">Class</label>
+                                            <input type="text" name="kkwpcf_class_message" id="kkwpcf_class_message" placeholder="Class">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfIdMessage">Id</label>
-                                            <input type="text" name="kkwpcfIdMessage" id="kkwpcfIdMessage" placeholder="Id">
+                                            <label for="kkwpcf_id_message">Id</label>
+                                            <input type="text" name="kkwpcf_id_message" id="kkwpcf_id_message" placeholder="Id">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfMaxlenMessage">Max length</label>
-                                            <input type="number" min="1" max="99" name="kkwpcfMaxlenMessage" id="kkwpcfMaxlenMessage" placeholder="Max length">
+                                            <label for="kkwpcf_maxlen_message">Max length</label>
+                                            <input type="number" min="1" name="kkwpcf_maxlen_message" id="kkwpcf_maxlen_message" placeholder="Max length">
                                         </div>
                                         <div class="input-field">
-                                            <label for="kkwpcfPlaceholderMessage">Placeholder</label>
-                                            <input type="text" name="kkwpcfPlaceholderMessage" id="kkwpcfPlaceholderMessage" placeholder="Placeholder">
+                                            <label for="kkwpcf_placeholder_message">Placeholder</label>
+                                            <input type="text" name="kkwpcf_placeholder_message" id="kkwpcf_placeholder_message" placeholder="Placeholder">
                                         </div>
                                     </div>
                                 </div>
